2f0ff40eb4b9e0b6bfe19d2b9127d138a75e8dd5
erb16 <erbayliss16@gmail.com>
2022-08-12 14:04:55 +0100

    profile page displays user's posts

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
-
+const Comment = require("../models/comment");
+const Post = require("../models/post");
 
 const UsersController = {
   New: (req, res) => {
@@ -20,7 +21,26 @@ const UsersController = {
   },
 
   Profile: (req, res) => {
-    User.findOne({ _id: req.params.id }).populate("posts").exec((err, users) => {
+
+    // User.find(query)
+    // .populate({ 
+    //    path: 'pages',
+    //    populate: {
+    //      path: 'components',
+    //      model: 'Component'
+    //    } 
+    // })
+    // .exec(function(err, docs) {});
+
+    User.findOne({ _id: req.params.id })
+    .populate({
+      path: "posts",
+      populate: {
+        path: "comments",
+        model: 'Comment'
+      }
+    })      
+    .exec((err, users) => {
       console.log(req.params.id)
       if (err) {
         throw err;
@@ -28,19 +48,45 @@ const UsersController = {
       res.render("users/:id", { users: users });
     })
 
+  },
 
-  }
+    Comments: (req, res) => {
+      // saves new comment into variable
+      const comment = new Comment(req.body)
+      // saves open session's email address into writer variable
+      let writer = req.session.user._id;
+      // accesses writer field for this new comment, saves the writer variable 
+      // (the current session's email address) inside
+      comment.writer = writer;
+      let nameOfWriter = req.session.user.first_name
+      let surnameOfWriter = req.session.user.surname
+      comment.nameOfWriter = `${nameOfWriter} ${surnameOfWriter}`
+      // finds the post within posts collection by id
+      Post.findById(req.params.id, function (err, docs) {
+      if (err){
+          console.log(err);
+      }
+      else{
+          // pushes new comment id inside the comments array for the post
+          docs.comments.push(comment);
+          // saves update
+          docs.save()
+      }
+     
+      });
+  
+  
+      comment.save((err, users) => {
+        if (err) {
+          throw err;
+        }
+    
+        res.status(201).redirect(window.history.back());
+    });
+    },
+  
 
-  // addFriend: (req, res) => {
-  //   User.findOne({ email: req.params.id }).populate("friends").exec((err, users) => {
-  //     if (err) {
-  //       throw err;
-  //     }
-  //     res.render("users/:id", { users: users });
-  //   })
-
-
-  // }
+  
 
 };
 
