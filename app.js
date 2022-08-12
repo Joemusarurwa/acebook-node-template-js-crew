0ac2e398c9fdc7c4446c794f1b104a3d8809fa1d
Joemusarurwa <joe.musarurwa@ft.com>
2022-08-12 12:12:23 +0100

    profile images

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const session = require("express-session");
 const methodOverride = require("method-override");
+const multer = require('multer');
 
 
 const homeRouter = require("./routes/home");
