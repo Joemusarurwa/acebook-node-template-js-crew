7d19cfbc1fda93752226554fd0533c17316d174f
Joemusarurwa <joe.musarurwa@ft.com>
2022-08-12 12:16:12 +0100

    profile images

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,16 @@ const multer = require('multer');
 
 const helpers = require("./public/javascripts/helpers");
 
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+      cb(null, 'public/images/');
+  },
+
+  // By default, multer removes file extensions so let's add them back
+  filename: function(req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
 
 const UsersController = {
   New: (req, res) => {
